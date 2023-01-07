8fc5d4b9aa1367eceb9df8f281d88c7423e3575f
許瑄筑 <hsuanchu8634@gmail.com>
2023-01-07 20:56:27 +0800

    add 230107

---
+++ 230107-Trajectory-of-The-Ball/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(100); //0:balck
+}
+
+let x = 50, y = 50;
+let vx = 3, vy = 3;
+p = 0.9
+
+function draw() {
+	//background(100);
+	ellipse(x, y, 50);
+	x = x + vx;
+	y = y + vy;
+	vy += 0.2;
+	if (y > height){
+		vy = -vy * p; //ball bounce
+		y = height; //Avoid p is too small to cause the ball stick at the bottom.
+	}
+}
